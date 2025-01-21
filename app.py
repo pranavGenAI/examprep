974a86521a2066289838b956b03abcc02eb3c9e7
pranavGenAI <167668235+pranavGenAI@users.noreply.github.com>
2025-01-21 12:59:50 +0530

    app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def show_home_page():
 
     # Module Selection
     if st.session_state.selected_section:
-        st.header(f"Select Module for {st.session_state.selected_section}")
+        #st.header(f"Select Module for {st.session_state.selected_section}")
         columns = st.columns(3)
         for idx, module in enumerate(section_modules[st.session_state.selected_section]):
             with columns[idx % 3]:
